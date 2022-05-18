b6b693b445362738cacaf0f93789509988ea1bf3
mylogi <mp.gplay@gmail.com>
2022-05-18 11:42:36 +0300

    :sparkles: Added BaseStateData and Tip.
    1) BaseStateData have data for everything callback in menu.
    2) Tip have three heirs.
    3) Remake typing with 'BaseState'.

--- states/hello.py
+++ states/hello.py
@@ -3,7 +3,40 @@ from telebot import types
 from states.base import BaseState
 
 
-class Hello(BaseState):
+class BaseStateData(BaseState):
+
+    def process_call_back(self, message: types.CallbackQuery):
+        if message.data:
+            if message.data == 'next state: Hello':
+                return Hello
+            if message.data == 'next state: AvailableCVTemplates':
+                return AvailableCVTemplates
+            if message.data == 'next state: CVTips':
+                return FirstTip
+            if message.data == 'next state: FirstTemplate':
+                return FirstTemplate
+            if message.data == 'next state: SecondTemplate':
+                return SecondTemplate
+            if message.data == 'next state: CreateCV':
+                return CreateCV
+            if message.data == 'next state: CVTipsMenu':
+                return CVTipsMenu
+        return self.__class__
+
+
+class Tip(BaseState):
+
+    def get_keyboard(self):
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(
+            types.InlineKeyboardButton(text='Next tip', callback_data='next state: NextTip'),
+            types.InlineKeyboardButton(text='Previous tip', callback_data='next state: PreviousTip')
+        )
+        keyboard.add(types.InlineKeyboardButton(text='CV Tips Menu', callback_data='next state: CVTipsMenu'))
+        return keyboard
+
+
+class Hello(BaseStateData):
     text = "<b>Welcome to CVLite!</b>"
 
     def get_keyboard(self):
@@ -12,37 +45,19 @@ class Hello(BaseState):
         keyboard.add(types.InlineKeyboardButton(text='CV Tips', callback_data='next state: CVTips'))
         return keyboard
 
-    def process_call_back(self, message: types.CallbackQuery) -> 'BaseState':
-        if message.data:
-            if message.data == 'next state: AvailableCVTemplates':
-                return AvailableCVTemplates
-            if message.data == 'next state: CVTips':
-                return CVTipsMenu
-        return Hello
 
-
-class AvailableCVTemplates(BaseState):
+class AvailableCVTemplates(BaseStateData):
     text = "<b>CV Templates</b> \n\n Which one will you choose?"
 
     def get_keyboard(self):
         keyboard = types.InlineKeyboardMarkup()
         keyboard.add(types.InlineKeyboardButton(text='1 template', callback_data='next state: FirstTemplate'))
         keyboard.add(types.InlineKeyboardButton(text='2 template', callback_data='next state: SecondTemplate'))
-        keyboard.add(types.InlineKeyboardButton(text='Back', callback_data='next state: Hello'))
+        keyboard.add(types.InlineKeyboardButton(text='CVLite', callback_data='next state: Hello'))
         return keyboard
 
-    def process_call_back(self, message: types.CallbackQuery) -> 'BaseState':
-        if message.data:
-            if message.data == 'next state: FirstTemplate':
-                return FirstTemplate
-            if message.data == 'next state: SecondTemplate':
-                return SecondTemplate
-            if message.data == 'next state: Hello':
-                return Hello
-            return AvailableCVTemplates
 
-
-class FirstTemplate(BaseState):
+class FirstTemplate(BaseStateData):
     text = "<b>First template</b>"
 
     def get_keyboard(self):
@@ -51,16 +66,8 @@ class FirstTemplate(BaseState):
         keyboard.add(types.InlineKeyboardButton(text='CV Templates', callback_data='next state: AvailableCVTemplates'))
         return keyboard
 
-    def process_call_back(self, message: types.CallbackQuery) -> 'BaseState':
-        if message.data:
-            if message.data == 'next state: CreateCV':
-                return CreateCV
-            if message.data == 'next state: AvailableCVTemplates':
-                return AvailableCVTemplates
-            return FirstTemplate
 
-
-class SecondTemplate(BaseState):
+class SecondTemplate(BaseStateData):
     text = "<b>Second template</b>"
 
     def get_keyboard(self):
@@ -69,55 +76,68 @@ class SecondTemplate(BaseState):
         keyboard.add(types.InlineKeyboardButton(text='CV Templates', callback_data='next state: AvailableCVTemplates'))
         return keyboard
 
-    def process_call_back(self, message: types.CallbackQuery) -> 'BaseState':
-        if message.data:
-            if message.data == 'next state: CreateCV':
-                return CreateCV
-            if message.data == 'next state: AvailableCVTemplates':
-                return AvailableCVTemplates
-            return SecondTemplate
 
-
-class CVTipsMenu(BaseState):
-    text = '<b>\t CV Tips</b> \n\n What is needed for CV? Read on!'
+class CVTipsMenu(BaseStateData):
+    text = '<b>\t CV Tips Menu</b> \n\n What is needed for CV? Read on!'
 
     def get_keyboard(self):
         keyboard = types.InlineKeyboardMarkup()
-        keyboard.add(
-            types.InlineKeyboardButton(text='Next tip', callback_data='next state: NextTip'),
-            types.InlineKeyboardButton(text='Previous tip', callback_data='next state: PreviousTip')
-        )
+        keyboard.add(types.InlineKeyboardButton(text='CV Tips', callback_data='next state: CVTips'))
         keyboard.add(types.InlineKeyboardButton(text='CV Templates', callback_data='next state: AvailableCVTemplates'))
-        keyboard.add(types.InlineKeyboardButton(text='Back', callback_data='next state: Hello'))
+        keyboard.add(types.InlineKeyboardButton(text='CVLite', callback_data='next state: Hello'))
         return keyboard
 
-    def process_call_back(self, message: types.CallbackQuery) -> 'BaseState':
+
+class FirstTip(Tip):
+    text = "First tip"
+
+    def process_call_back(self, message: types.CallbackQuery):
         if message.data:
             if message.data == 'next state: NextTip':
-                pass
+                return SecondTip
             if message.data == 'next state: PreviousTip':
-                pass
-            if message.data == 'next state: AvailableCVTemplates':
-                return AvailableCVTemplates
-            if message.data == 'next state: Hello':
-                return Hello
-        return self
-
-    def send_tip(self, number):
-        self.bot.send_message(
-            self.chat_id, self.image_name_dictionary[number], parse_mode='HTML')
-        self.bot.send_message(
-            self.chat_id, self.tips_dictionary[number], parse_mode='HTML')
+                return ThirdTip
+            if message.data == 'next state: CVTipsMenu':
+                return CVTipsMenu
+        return FirstTip
 
 
-class CreateCV(BaseState):
+class SecondTip(Tip):
+    text = "Second tip"
+
+    def process_call_back(self, message: types.CallbackQuery):
+        if message.data:
+            if message.data == 'next state: NextTip':
+                return ThirdTip
+            if message.data == 'next state: PreviousTip':
+                return FirstTip
+            if message.data == 'next state: CVTipsMenu':
+                return CVTipsMenu
+        return SecondTip
+
+
+class ThirdTip(Tip):
+    text = "Third tip"
+
+    def process_call_back(self, message: types.CallbackQuery):
+        if message.data:
+            if message.data == 'next state: NextTip':
+                return FirstTip
+            if message.data == 'next state: PreviousTip':
+                return SecondTip
+            if message.data == 'next state: CVTipsMenu':
+                return CVTipsMenu
+        return ThirdTip
+
+
+class CreateCV(BaseStateData):
     text = "<b>Create CV</b>\n\n Follow the instructions to create a CV.\n\n Good luck!"
 
 
 class ActionState(BaseState):
     text = 'Sale example: 1 - New Year, 2 - More minutes, 3 - Soon return'
 
-    def process_text_message(self, message: types.Message) -> 'BaseState':
+    def process_text_message(self, message: types.Message):
         if message.text in ('1', '2', '3'):
             return ActionAppliedState
         self.send_warning('Press 1, 2 or 3!')
@@ -132,7 +152,7 @@ class ActionAppliedState(BaseState):
         keyboard.add(types.InlineKeyboardButton(text='Home', callback_data='next state: Hello'))
         return keyboard
 
-    def process_call_back(self, message: types.CallbackQuery) -> 'BaseState':
+    def process_call_back(self, message: types.CallbackQuery):
         if message.data and message.data == 'next state: Hello':
             return Hello
         return ActionAppliedState
