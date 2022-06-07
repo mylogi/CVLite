68c469b918cd4727fc633de632003e06f5ed83b3
mylogi <mp.gplay@gmail.com>
2022-06-07 09:50:35 +0300

    Deploy on Heroku.

---
+++ testing/pil_action.py
@@ -0,0 +1,43 @@
+# from PIL import Image, ImageDraw
+
+# bg = Image.new("RGBA", (400, 400))
+
+# msk = Image.new("RGBA", (400, 400))
+#
+# mdr = ImageDraw.Draw(msk)
+
+# img = Image.open('CV.jpg')
+# img = Image.new('RGBA', (500, 500))
+# x = img.size
+# if x[0] / x[1] == 1.0:
+#     img.thumbnail((170, 170))
+#     img.save('kek.png')
+# else:
+#     z = x[1] - x[0]
+#     y = x[1] - z
+#     ss = img.crop((0, 0, x[0], y))
+#     ss.thumbnail((170, 170))
+#     ss.save('lolo.png')
+# img.thumbnail((500, 500))
+# ss = img.crop((0, 0, 400, 400))
+# ss.thumbnail((170, 170))
+# ss.save('lolo.png')
+
+# img2 = Image.open('avatar_new.png').convert("RGBA")
+# img3 = Image.open('new_avatar.png')
+#
+# img3.thumbnail((170, 170))
+# img3.save('new_avatar_170.png')
+
+# img.thumbnail((170, 170))
+# img.save('elipse_mask_normal_170.png')
+#
+# img2.thumbnail((400, 400))
+#
+# img.save('CV_bot.jpg')
+
+# bg.paste(img2, (0, 0), img)
+# #
+# bg.show()
+#
+# bg.save('new_avatar.png')
